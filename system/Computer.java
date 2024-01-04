90263fe6250d20795f392a0a0190b6c8446003dc
DivyaDharshiniI-07 <153634531+DivyaDharshiniI-07@users.noreply.github.com>
2024-01-04 22:41:54 +0530

    Add files via upload

---
+++ system/Computer.java
@@ -0,0 +1,15 @@
+package org.system;
+
+public class Computer {
+	public void computerModel() {
+		System.out.println("Lenovo");
+	}
+
+	public void windowsVersion() {
+		System.out.println("Windows 11");
+	}
+
+	public void storage() {
+		System.out.println("8gb RAM");
+	}
+}
